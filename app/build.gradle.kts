fe4e8fcada0cfb6d2004ae7cef787af5f2c8de2a
anton bogdanov <a.bogdanov@evotor.ru>
2024-07-01 09:25:56 +0300

    cleaning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-//    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -13,8 +12,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -40,12 +37,4 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-//    kotlinOptions {
-//        jvmTarget = "1.8"
-//    }
-}
-
-dependencies {
-
-//    implementation("androidx.core:core-ktx:1.13.1")
 }
